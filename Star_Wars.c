ef7dd00e8f90587afd58c958cbf0589c06431563
Santiago Salvioli <santiagogsalvioli@gmail.com>
2021-05-04 19:33:29 -0300

    Add files via upload

---
+++ Star_Wars.c
@@ -0,0 +1,44 @@
+
+#include<stdio.h>
+#include<windows.h>
+int main(void)
+{
+	char a[]=" 888888                       8   8  8                    ";
+	char b[]="8      eeeee eeeee eeeee     8   8  8 eeeee eeeee  eeeee ";
+	char c[]="8eeeee   8   8   8 8   8     8e  8  8 8   8 8   8  8     ";
+	char d[]="    88   8e  8eee8 8eee8e    88  8  8 8eee8 8eee8e 8eeee ";
+	char e[]="e   88   88  88  8 88   8    88  8  8 88  8 88   8    88 ";
+	char f[]="8eee88   88  88  8 88   8    88ee8ee8 88  8 88   8 8ee88 ";
+	char g[]="";
+	char h[]="88888                             ";
+	char i[]="8     eeeee e eeeee e  eeeee eeeee";
+	char j[]="8eeee 8   8 8   8   8  8  88 8   8";
+	char k[]="88    8e  8 8e  8e  8e 8   8 8e  8";
+	char l[]="88    88  8 88  88  88 8   8 88  8";
+	char m[]="88eee 88ee8 88  88  88 8eee8 88  8";
+
+printf("%s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n %s\n ",a,b,c,d,e,f,g,h,i,j,k,l,m);
+printf("by Salvio\n");	
+	
+Beep(440,500);       
+Beep(440,500) ;
+Beep(440,500)  ;      
+Beep(349,350)   ;     
+Beep(523,150)    ;    
+Beep(440,500)     ;   
+Beep(349,350)      ;  
+Beep(523,150)       ; 
+Beep(440,1000) ;
+Beep(659,500)   ;     
+Beep(659,500)    ;    
+Beep(659,500)     ;   
+Beep(698,350)      ;  
+Beep(523,150)       ; 
+Beep(415,500)  ;
+Beep(349,350)  ;
+Beep(523,150) ;
+Beep(440,2000);
+//Beep(640,400);
+system("pause");
+return 0;
+}
